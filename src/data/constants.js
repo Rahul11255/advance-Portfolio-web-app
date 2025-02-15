d6a2c1fb7a68822f93a78c027b6c2c5d6b69b086
Rahul Patel <89196318+Rahul11255@users.noreply.github.com>
2025-02-15 23:21:59 +0530

    Update constants.js
    resume url change

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ orexport const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: " https://github.com/Rahul11255",
   resume:
-    "https://drive.google.com/file/d/1lbaWL21DPw-RJtA6OKKG309RfqxlwojQ/view",
+    "https://drive.google.com/file/d/10Suv-PF_5aPmaSu5O7zHilPfVQD0KFPP/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/rahul-patel-323baa213/",
   whatsapp: "https://wa.me/9354081946",
   insta: "https://www.instagram.com/rahulpatel11255/",
