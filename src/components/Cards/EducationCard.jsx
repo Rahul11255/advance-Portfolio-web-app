1f4faf93a35adab76736b1c5fd99d9a1591899f3
Rahul11255 <rr710505@gmail.com>
2026-08-19 21:30:49 +0530

    update

--- src/components/Cards/EducationCard.jsx
+++ src/components/Cards/EducationCard.jsx
@@ -132,14 +132,14 @@ const EducationCard = ({ education }) => {
     return (
         <Card>
             <Top>
-                <Image src={education.img} />
+                {/* <Image src={education.img} /> */}
                 <Body>
                     <Name>{education.school}</Name>
                     <Degree>{education.degree}</Degree>
                     <Date>{education.date}</Date>
                 </Body>
             </Top>
-            <Grade><b>Grade: </b>{education.grade}</Grade>
+            {/* <Grade><b>Grade: </b>{education.grade}</Grade> */}
             <Description>
                 <Span>{education.desc}</Span>
             </Description>
